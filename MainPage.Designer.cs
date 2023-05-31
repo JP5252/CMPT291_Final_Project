c62377e78d98d24b7715f7d58ab7ee636952bc62
JP5252 <justin.pearson52@hotmail.com>
2023-05-31 15:54:16 -0600

    Finish Car page GUI and Added Search and Add functionality

--- MainPage.Designer.cs
+++ MainPage.Designer.cs
@@ -28,12 +28,307 @@
         /// </summary>
         private void InitializeComponent()
         {
-            tabControl1 = new TabControl();
-            tabPage1 = new TabPage();
-            tabPage2 = new TabPage();
             tabPage3 = new TabPage();
+            tabPage2 = new TabPage();
+            tabPage1 = new TabPage();
+            LicensePlateTextBox = new TextBox();
+            label8 = new Label();
+            MileageTextBox = new TextBox();
+            label5 = new Label();
+            YearTextBox = new TextBox();
+            label6 = new Label();
+            RegistrationTextBox = new TextBox();
+            label7 = new Label();
+            ModelTextBox = new TextBox();
+            label4 = new Label();
+            MakeTextBox = new TextBox();
+            label3 = new Label();
+            CarIDTextBox = new TextBox();
+            label2 = new Label();
+            CTIDTextBox = new TextBox();
+            label1 = new Label();
+            SearchTextBox = new TextBox();
+            SearchComboBox = new ComboBox();
+            SearchBtn = new Button();
+            ModifyBtn = new Button();
+            DeleteBtn = new Button();
+            AddBtn = new Button();
+            Car = new DataGridView();
+            tabControl1 = new TabControl();
+            CarID = new DataGridViewTextBoxColumn();
+            Make = new DataGridViewTextBoxColumn();
+            Model = new DataGridViewTextBoxColumn();
+            Year = new DataGridViewTextBoxColumn();
+            Mileage = new DataGridViewTextBoxColumn();
+            Registration = new DataGridViewTextBoxColumn();
+            LicensePlate = new DataGridViewTextBoxColumn();
+            CTID = new DataGridViewTextBoxColumn();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)Car).BeginInit();
             tabControl1.SuspendLayout();
             SuspendLayout();
+            // 
+            // tabPage3
+            // 
+            tabPage3.Location = new Point(4, 34);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(891, 431);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Reports";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 34);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(891, 431);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Rentals";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(LicensePlateTextBox);
+            tabPage1.Controls.Add(label8);
+            tabPage1.Controls.Add(MileageTextBox);
+            tabPage1.Controls.Add(label5);
+            tabPage1.Controls.Add(YearTextBox);
+            tabPage1.Controls.Add(label6);
+            tabPage1.Controls.Add(RegistrationTextBox);
+            tabPage1.Controls.Add(label7);
+            tabPage1.Controls.Add(ModelTextBox);
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(MakeTextBox);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(CarIDTextBox);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(CTIDTextBox);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(SearchTextBox);
+            tabPage1.Controls.Add(SearchComboBox);
+            tabPage1.Controls.Add(SearchBtn);
+            tabPage1.Controls.Add(ModifyBtn);
+            tabPage1.Controls.Add(DeleteBtn);
+            tabPage1.Controls.Add(AddBtn);
+            tabPage1.Controls.Add(Car);
+            tabPage1.Location = new Point(4, 34);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(891, 431);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Cars";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // LicensePlateTextBox
+            // 
+            LicensePlateTextBox.Location = new Point(580, 316);
+            LicensePlateTextBox.Name = "LicensePlateTextBox";
+            LicensePlateTextBox.Size = new Size(150, 31);
+            LicensePlateTextBox.TabIndex = 20;
+            // 
+            // label8
+            // 
+            label8.Anchor = AnchorStyles.Top;
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(485, 322);
+            label8.Name = "label8";
+            label8.Size = new Size(95, 21);
+            label8.TabIndex = 21;
+            label8.Text = "LicensePlate";
+            // 
+            // MileageTextBox
+            // 
+            MileageTextBox.Location = new Point(334, 335);
+            MileageTextBox.Name = "MileageTextBox";
+            MileageTextBox.Size = new Size(150, 31);
+            MileageTextBox.TabIndex = 16;
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Top;
+            label5.AutoSize = true;
+            label5.Location = new Point(254, 334);
+            label5.Name = "label5";
+            label5.Size = new Size(74, 25);
+            label5.TabIndex = 19;
+            label5.Text = "Mileage";
+            // 
+            // YearTextBox
+            // 
+            YearTextBox.Location = new Point(334, 292);
+            YearTextBox.Name = "YearTextBox";
+            YearTextBox.Size = new Size(150, 31);
+            YearTextBox.TabIndex = 14;
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.Top;
+            label6.AutoSize = true;
+            label6.Location = new Point(269, 291);
+            label6.Name = "label6";
+            label6.Size = new Size(44, 25);
+            label6.TabIndex = 17;
+            label6.Text = "Year";
+            // 
+            // RegistrationTextBox
+            // 
+            RegistrationTextBox.Location = new Point(334, 377);
+            RegistrationTextBox.Name = "RegistrationTextBox";
+            RegistrationTextBox.Size = new Size(150, 31);
+            RegistrationTextBox.TabIndex = 18;
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.Top;
+            label7.AutoSize = true;
+            label7.Location = new Point(228, 376);
+            label7.Name = "label7";
+            label7.Size = new Size(106, 25);
+            label7.TabIndex = 15;
+            label7.Text = "Registration";
+            // 
+            // ModelTextBox
+            // 
+            ModelTextBox.Location = new Point(73, 381);
+            ModelTextBox.Name = "ModelTextBox";
+            ModelTextBox.Size = new Size(150, 31);
+            ModelTextBox.TabIndex = 12;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            label4.AutoSize = true;
+            label4.Location = new Point(8, 380);
+            label4.Name = "label4";
+            label4.Size = new Size(63, 25);
+            label4.TabIndex = 13;
+            label4.Text = "Model";
+            // 
+            // MakeTextBox
+            // 
+            MakeTextBox.Location = new Point(73, 335);
+            MakeTextBox.Name = "MakeTextBox";
+            MakeTextBox.Size = new Size(150, 31);
+            MakeTextBox.TabIndex = 10;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            label3.AutoSize = true;
+            label3.Location = new Point(8, 334);
+            label3.Name = "label3";
+            label3.Size = new Size(55, 25);
+            label3.TabIndex = 11;
+            label3.Text = "Make";
+            // 
+            // CarIDTextBox
+            // 
+            CarIDTextBox.Location = new Point(73, 292);
+            CarIDTextBox.Name = "CarIDTextBox";
+            CarIDTextBox.Size = new Size(150, 31);
+            CarIDTextBox.TabIndex = 8;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            label2.AutoSize = true;
+            label2.Location = new Point(8, 291);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 25);
+            label2.TabIndex = 9;
+            label2.Text = "CarID";
+            // 
+            // CTIDTextBox
+            // 
+            CTIDTextBox.Location = new Point(580, 353);
+            CTIDTextBox.Name = "CTIDTextBox";
+            CTIDTextBox.Size = new Size(150, 31);
+            CTIDTextBox.TabIndex = 22;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top;
+            label1.AutoSize = true;
+            label1.Location = new Point(503, 355);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 25);
+            label1.TabIndex = 7;
+            label1.Text = "CTID";
+            // 
+            // SearchTextBox
+            // 
+            SearchTextBox.Anchor = AnchorStyles.Top;
+            SearchTextBox.Location = new Point(301, 219);
+            SearchTextBox.Name = "SearchTextBox";
+            SearchTextBox.Size = new Size(279, 31);
+            SearchTextBox.TabIndex = 6;
+            // 
+            // SearchComboBox
+            // 
+            SearchComboBox.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            SearchComboBox.FormattingEnabled = true;
+            SearchComboBox.Location = new Point(8, 219);
+            SearchComboBox.Name = "SearchComboBox";
+            SearchComboBox.Size = new Size(279, 33);
+            SearchComboBox.TabIndex = 5;
+            // 
+            // SearchBtn
+            // 
+            SearchBtn.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            SearchBtn.Location = new Point(608, 221);
+            SearchBtn.Name = "SearchBtn";
+            SearchBtn.Size = new Size(273, 33);
+            SearchBtn.TabIndex = 4;
+            SearchBtn.Text = "Search";
+            SearchBtn.UseVisualStyleBackColor = true;
+            SearchBtn.Click += SearchBtn_Click;
+            // 
+            // ModifyBtn
+            // 
+            ModifyBtn.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            ModifyBtn.Location = new Point(736, 316);
+            ModifyBtn.Name = "ModifyBtn";
+            ModifyBtn.Size = new Size(145, 50);
+            ModifyBtn.TabIndex = 3;
+            ModifyBtn.Text = "Modify";
+            ModifyBtn.UseVisualStyleBackColor = true;
+            // 
+            // DeleteBtn
+            // 
+            DeleteBtn.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            DeleteBtn.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            DeleteBtn.Location = new Point(736, 372);
+            DeleteBtn.Name = "DeleteBtn";
+            DeleteBtn.Padding = new Padding(5, 0, 5, 5);
+            DeleteBtn.Size = new Size(145, 50);
+            DeleteBtn.TabIndex = 2;
+            DeleteBtn.Text = "Delete";
+            DeleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // AddBtn
+            // 
+            AddBtn.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            AddBtn.Location = new Point(736, 260);
+            AddBtn.Name = "AddBtn";
+            AddBtn.Size = new Size(145, 50);
+            AddBtn.TabIndex = 1;
+            AddBtn.Text = "Add";
+            AddBtn.UseVisualStyleBackColor = true;
+            AddBtn.Click += AddBtn_Click;
+            // 
+            // Car
+            // 
+            Car.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            Car.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            Car.Columns.AddRange(new DataGridViewColumn[] { CarID, Make, Model, Year, Mileage, Registration, LicensePlate, CTID });
+            Car.Location = new Point(6, 6);
+            Car.Name = "Car";
+            Car.RowHeadersWidth = 62;
+            Car.RowTemplate.Height = 33;
+            Car.Size = new Size(875, 191);
+            Car.TabIndex = 0;
             // 
             // tabControl1
             // 
@@ -48,34 +343,61 @@
             tabControl1.SizeMode = TabSizeMode.FillToRight;
             tabControl1.TabIndex = 0;
             // 
-            // tabPage1
+            // CarID
             // 
-            tabPage1.Location = new Point(4, 34);
-            tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(891, 431);
-            tabPage1.TabIndex = 0;
-            tabPage1.Text = "Cars";
-            tabPage1.UseVisualStyleBackColor = true;
+            CarID.HeaderText = "CarID";
+            CarID.MinimumWidth = 8;
+            CarID.Name = "CarID";
+            CarID.Width = 80;
             // 
-            // tabPage2
+            // Make
             // 
-            tabPage2.Location = new Point(4, 34);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(891, 431);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "Rentals";
-            tabPage2.UseVisualStyleBackColor = true;
+            Make.HeaderText = "Make";
+            Make.MinimumWidth = 8;
+            Make.Name = "Make";
+            Make.Width = 120;
             // 
-            // tabPage3
+            // Model
             // 
-            tabPage3.Location = new Point(4, 34);
-            tabPage3.Name = "tabPage3";
-            tabPage3.Size = new Size(891, 431);
-            tabPage3.TabIndex = 2;
-            tabPage3.Text = "Reports";
-            tabPage3.UseVisualStyleBackColor = true;
+            Model.HeaderText = "Model";
+            Model.MinimumWidth = 8;
+            Model.Name = "Model";
+            Model.Width = 120;
+            // 
+            // Year
+            // 
+            Year.HeaderText = "Year";
+            Year.MinimumWidth = 8;
+            Year.Name = "Year";
+            Year.Width = 80;
+            // 
+            // Mileage
+            // 
+            Mileage.HeaderText = "Mileage";
+            Mileage.MinimumWidth = 8;
+            Mileage.Name = "Mileage";
+            Mileage.Width = 120;
+            // 
+            // Registration
+            // 
+            Registration.HeaderText = "Registration";
+            Registration.MinimumWidth = 8;
+            Registration.Name = "Registration";
+            Registration.Width = 120;
+            // 
+            // LicensePlate
+            // 
+            LicensePlate.HeaderText = "LicensePlate";
+            LicensePlate.MinimumWidth = 8;
+            LicensePlate.Name = "LicensePlate";
+            LicensePlate.Width = 110;
+            // 
+            // CTID
+            // 
+            CTID.HeaderText = "CTID";
+            CTID.MinimumWidth = 8;
+            CTID.Name = "CTID";
+            CTID.Width = 50;
             // 
             // MainPage
             // 
@@ -86,15 +408,49 @@
             Controls.Add(tabControl1);
             Name = "MainPage";
             Text = "Car Rentals";
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)Car).EndInit();
             tabControl1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private TabControl tabControl1;
-        private TabPage tabPage1;
-        private TabPage tabPage2;
         private TabPage tabPage3;
+        private TabPage tabPage2;
+        private TabPage tabPage1;
+        private TextBox SearchTextBox;
+        private ComboBox SearchComboBox;
+        private Button SearchBtn;
+        private Button ModifyBtn;
+        private Button DeleteBtn;
+        private Button AddBtn;
+        private DataGridView Car;
+        private TabControl tabControl1;
+        private Label label1;
+        private TextBox ModelTextBox;
+        private Label label4;
+        private TextBox MakeTextBox;
+        private Label label3;
+        private TextBox CarIDTextBox;
+        private Label label2;
+        private TextBox CTIDTextBox;
+        private TextBox MileageTextBox;
+        private Label label5;
+        private TextBox YearTextBox;
+        private Label label6;
+        private TextBox RegistrationTextBox;
+        private Label label7;
+        private TextBox LicensePlateTextBox;
+        private Label label8;
+        private DataGridViewTextBoxColumn CarID;
+        private DataGridViewTextBoxColumn Make;
+        private DataGridViewTextBoxColumn Model;
+        private DataGridViewTextBoxColumn Year;
+        private DataGridViewTextBoxColumn Mileage;
+        private DataGridViewTextBoxColumn Registration;
+        private DataGridViewTextBoxColumn LicensePlate;
+        private DataGridViewTextBoxColumn CTID;
     }
 }
--- MainPage.cs
+++ MainPage.cs
@@ -1,10 +1,79 @@
+using Azure;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
 namespace CMPT291_Final_Project
 {
     public partial class MainPage : Form
     {
+        public SqlConnection myConnection;
+        public SqlCommand myCommand;
+        public SqlDataReader myReader;
+
         public MainPage()
         {
             InitializeComponent();
+            ///////////////////////////////
+            SearchComboBox.Items.Clear();
+            SearchComboBox.Items.Add("Show all");
+            //////////////////////////////////
+
+            String connectionString = "Server = DESKTOP-733UFP1; Database = CMPT_291_FinalProject; Trusted_Connection = yes;";
+
+            SqlConnection myConnection = new SqlConnection(connectionString); // Timeout in seconds
+
+            try
+            {
+                myConnection.Open(); // Open connection
+                myCommand = new SqlCommand();
+                myCommand.Connection = myConnection; // Link the command stream to the connection
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error");
+                this.Close();
+            }
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                myCommand.CommandText = "insert into Car values (" + CarIDTextBox.Text + ",'" + MakeTextBox.Text + "','" + ModelTextBox.Text + "'," + YearTextBox.Text +
+                                         "," + MileageTextBox.Text + ",'" + RegistrationTextBox.Text + "','" + LicensePlateTextBox.Text + "','" + CTIDTextBox.Text + "')";
+                MessageBox.Show(myCommand.CommandText);
+
+                myCommand.ExecuteNonQuery();
+            }
+            catch (Exception e2)
+            {
+                MessageBox.Show(e2.ToString(), "Error");
+            }
+        }
+
+        private void SearchBtn_Click(object sender, EventArgs e)
+        {
+            myCommand.CommandText = "select * from Car";
+
+            try
+            {
+                MessageBox.Show(myCommand.CommandText);
+                myReader = myCommand.ExecuteReader();
+
+                Car.Rows.Clear();
+                while (myReader.Read())
+                {
+                    Car.Rows.Add(myReader["CarID"].ToString(), myReader["Make"].ToString(), myReader["Model"].ToString(), myReader["Year"].ToString(),
+                    myReader["Mileage"].ToString(), myReader["Registration"].ToString(), myReader["LicensePlate"].ToString(), myReader["CTID"].ToString());
+                }
+
+                myReader.Close();
+            }
+            catch (Exception e3)
+            {
+                MessageBox.Show(e3.ToString(), "Error");
+            }
         }
     }
 }
