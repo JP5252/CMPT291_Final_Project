5d48cf95e1609d3ecc594d6a80794facc8612597
PenutButer <itzjustbalony@gmail.com>
2023-05-31 20:22:10 -0600

    Implemented modification of cars

--- MainPage.cs
+++ MainPage.cs
@@ -18,7 +18,7 @@ namespace CMPT291_Final_Project
             SearchComboBox.Items.Add("Show all");
             //////////////////////////////////
 
-            String connectionString = "Server = DESKTOP-733UFP1; Database = CMPT_291_FinalProject; Trusted_Connection = yes;";
+            String connectionString = "Server = DESKTOP-JOHN; Database = CMPT_291_FinalProject; Trusted_Connection = yes;";
 
             SqlConnection myConnection = new SqlConnection(connectionString); // Timeout in seconds
 
@@ -27,6 +27,7 @@ namespace CMPT291_Final_Project
                 myConnection.Open(); // Open connection
                 myCommand = new SqlCommand();
                 myCommand.Connection = myConnection; // Link the command stream to the connection
+
             }
             catch (Exception e)
             {
@@ -39,26 +40,84 @@ namespace CMPT291_Final_Project
         {
             try
             {
-
                 myCommand.CommandText = "insert into Car values (" + CarIDTextBox.Text + ",'" + MakeTextBox.Text + "','" + ModelTextBox.Text + "'," + YearTextBox.Text +
                                          "," + MileageTextBox.Text + ",'" + RegistrationTextBox.Text + "','" + LicensePlateTextBox.Text + "','" + CTIDTextBox.Text + "')";
                 MessageBox.Show(myCommand.CommandText);
 
                 myCommand.ExecuteNonQuery();
+                myCommand.Dispose();
             }
             catch (Exception e2)
             {
                 MessageBox.Show(e2.ToString(), "Error");
             }
+
+            // Show all cars after adding one
+            ShowAllCars();
+        }
+
+        private void ModifyBtn_Click(object sender, EventArgs e)
+        {
+            String SelectedID = CarIDTextBox.Text;
+            myCommand.CommandText = $"SELECT * FROM Car WHERE CarID = {SelectedID}";
+            SqlDataReader Data = myCommand.ExecuteReader();
+
+            if (Data.Read())
+            {
+                String Changes = (
+                    $"Please confirm these changes for Car ID {CarIDTextBox.Text} (Old -> New):\n\n" +
+                    $"Make: {Data["Make"]} -> {MakeTextBox.Text}\n" +
+                    $"Model: {Data["Model"]} -> {ModelTextBox.Text}\n" +
+                    $"Year: {Data["Year"]} -> {YearTextBox.Text}\n" +
+                    $"Mileage: {Data["Mileage"]} -> {MileageTextBox.Text}\n" +
+                    $"Registration: {Data["Registration"]} -> {RegistrationTextBox.Text}\n" +
+                    $"License Plate: {Data["LicensePlate"]} -> {LicensePlateTextBox.Text}\n" +
+                    $"CTID: {Data["CTID"]} -> {CTIDTextBox.Text}"
+                );
+
+                DialogResult ConfirmModification = MessageBox.Show(
+                Changes, "Confirmation", MessageBoxButtons.YesNo
+                );
+
+                Data.Close();
+
+                if (ConfirmModification == DialogResult.Yes)
+                {
+                    // Modify row
+                    myCommand.CommandText = (
+                        $"UPDATE Car " +
+                        $"SET Make = '{MakeTextBox.Text}', " +
+                        $"Model = '{ModelTextBox.Text}', " +
+                        $"Year = {YearTextBox.Text}, " +
+                        $"Mileage = {MileageTextBox.Text}, " +
+                        $"Registration = '{RegistrationTextBox.Text}', " +
+                        $"LicensePlate = '{LicensePlateTextBox.Text}', " +
+                        $"CTID = '{CTIDTextBox.Text}' " +
+                        $"WHERE CarID = {SelectedID};"
+                    );
+                    myCommand.ExecuteNonQuery();
+                    myCommand.Dispose();
+                }
+            }
+            ShowAllCars();
         }
 
         private void SearchBtn_Click(object sender, EventArgs e)
         {
-            myCommand.CommandText = "select * from Car";
 
+            // if selection is "Show all" call ShowAllCars()
+            if (SearchComboBox.Text == "Show all")
+            {
+                ShowAllCars();
+            }
+        }
+
+        private void ShowAllCars()
+        {
+            myCommand.CommandText = "select * from Car";
             try
             {
-                MessageBox.Show(myCommand.CommandText);
+                MessageBox.Show($"Query used: {myCommand.CommandText}");
                 myReader = myCommand.ExecuteReader();
 
                 Car.Rows.Clear();
@@ -75,5 +134,6 @@ namespace CMPT291_Final_Project
                 MessageBox.Show(e3.ToString(), "Error");
             }
         }
+
     }
 }
